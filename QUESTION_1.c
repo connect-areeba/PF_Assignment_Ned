a0af0dfdc4f8a37fcd941347c19e3ee3cab0f8c5
Areeba <connect.areeba@gmail.com>
2025-09-23 16:50:37 +0100

    Add files via upload

---
+++ QUESTION_1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+	
+	char letter;
+	
+	printf("The Letter is:");
+	scanf("%c", &letter);
+	
+	if ( letter >= 'A' && letter <= 'Z' ){
+		printf("The letter is Capital");
+	}
+	
+	else if ( letter >= 'a' && letter <= 'z' ){
+		printf("The letter is Small");
+	}
+	
+	else if (letter >= '0' && letter <= '9'){
+		printf("It is a Digit");
+	}
+	
+	else
+	   printf("Its a special character");
+	   
+	   return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
