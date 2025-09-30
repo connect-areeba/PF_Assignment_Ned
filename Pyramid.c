a0eb8c9fd1bfa7c0bd852869a0cb2a028532fc40
Areeba <connect.areeba@gmail.com>
2025-09-30 20:57:02 +0500

    Create Pyramid.c

---
+++ Pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;      
+    int i, j, space;
+
+    for(i = 1; i <= rows; i++) {
+        for(space = 1; space <= rows - i; space++) {
+            printf("  ");  
+        }
+        for(j = 1; j <= 2*i - 1; j++) {
+            printf("* ");
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
