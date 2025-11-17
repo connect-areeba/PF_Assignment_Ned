924900aee5a2d3ba59ef76cd0c9713bc7d4f4001
Areeba <connect.areeba@gmail.com>
2025-11-17 23:34:02 +0500

    Add files via upload

---
+++ ITFA Lab 11 Python/Word"Python".py
@@ -0,0 +1,6 @@
+sentence = input("Enter a sentence: ")
+sentence = sentence.lower()
+if "python" in sentence:
+    print("yes! the sentence contain the word 'python'")
+else: 
+    print("No! the word 'python' is not in sentence")
